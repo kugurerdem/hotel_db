5467c24c53be6085877bfd17dc4d4c537641500d
Uğur Erdem Seyfi <k.ugurerdem@gmail.com>
2021-05-12 21:37:06 +0300

    minor update

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config()
+
+const dbService = require('./dbService');
+const { response } = require('express');
+
+app.use( cors())
+app.use( express.json())
+app.use( express.urlencoded( {extended: false}))
 
 // GET
 app.get("/", (req, res) => {
     res.send('Hello World!!!');
+})
+
+app.get('/getAll', (req, response) => {
+    const db = dbService.getDbServiceInstance();
+    const result = db.getAllData();
+
+    result
+    .then( data => response.json({ data: data}))
+    .catch( err => console.log(err) );
 })
 
 // POST
